bfd7e27cd23b885fa824f416e8248a3dee5e751e
guoshunfa <16603684608@guoshunfa.com>
2025-02-19 09:16:27 +0800

    Initial commit

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    text: '指南', 
+    link: '/pages/a2f161', 
+    items: [
+      { text: '介绍', link: '/pages/a2f161/' },
+      { text: '快速上手', link: '/pages/793dcb/' },
+      { text: '目录结构', link: '/pages/2f674a/' },
+    ]
+  },
+  {
+    text: '参考', 
+    items: [
+      { text: '配置1', link: '/pages/d09980/' },
+      { text: '配置2', link: '/pages/d09980/' },
+    ]
+  },
+  { text: '关于', link: '/pages/0fa93a/' },
+  { text: '赞助', link: '/pages/0084a5/' },
+] 
