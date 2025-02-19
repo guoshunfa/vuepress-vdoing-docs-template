26d92073b947bd7dbdb5086f85898d62eb495989
guoshunfa <16603684608@guoshunfa.com>
2025-02-19 10:08:00 +0800

    调整config.js中base配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,13 @@
 const htmlModules = require('./config/htmlModules.js');
 const nav = require('./config/nav.js');
 
-
 module.exports = {
   theme: 'vdoing', // 使用依赖包主题
   // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
 
   title: "vuepress-vdoing-docs-template",
   description: '一个基于vuepress-vdoing的文档模板',
-  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: '/vuepress-vdoing-docs-template/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/111.jpg' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
